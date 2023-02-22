2f4dc8fed52d8759351e0d300922a56e83726842
liulei11 <liulei11@kuaishou.com>
2023-02-22 14:37:47 +0800

    feat: 修改 format 逻辑

--- src/_utils/format.ts
+++ src/_utils/format.ts
@@ -49,7 +49,7 @@ export const spliceDataFormatString = (entity: Entity, entities: Entity[]) => {
 		.filter(field => field.bizType !== FieldBizType.MAPPING && field.selected)
 		.forEach(filed => {
 			if (filed.bizType === FieldBizType.DATETIME && filed.showFormat) {
-				ifString += `;if (key === '${filed.name}') { item['_' + key] = item[key]; item[key] = FORMAT_MAP.formatTime(new Date(item[key]), '${filed.showFormat}') }\n`;
+				ifString += `;if (key === '${filed.name}') { item['_' + key] = item[key]; item[key] = item[key] ? FORMAT_MAP.formatTime(new Date(item[key]), '${filed.showFormat}') : item[key]; }\n`;
 			}
 		});
 	/** mapping 字段列表 */
@@ -61,7 +61,7 @@ export const spliceDataFormatString = (entity: Entity, entities: Entity[]) => {
 		
 		entity.fieldAry.forEach(f => {
 			if (f.bizType === FieldBizType.DATETIME && f.showFormat) {
-				ifString += `;if (key === '${field.name}_${f.name}') { item['_' + key] = item[key]; item[key] = FORMAT_MAP.formatTime(new Date(item[key]), '${f.showFormat}') }\n`;
+				ifString += `;if (key === '${field.name}_${f.name}') { item['_' + key] = item[key]; item[key] = item[key] ? FORMAT_MAP.formatTime(new Date(item[key]), '${f.showFormat}') : item[key]; }\n`;
 			}
 			
 			convertString += `item.${field.name}.${f.name} = item.${field.name}_${f.name};
